ca86759fe64860ca073d00dc88263511055ccaab
Chris Guard <cguard90@gmail.com>
2016-02-15 18:07:03 -0500

    add entires show route

--- blog2/config/routes.rb
+++ blog2/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
   get 'links' => 'links#index'
+  resources :entries,  only: [:show]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
