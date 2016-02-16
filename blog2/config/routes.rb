49b5be83542f90c698f477d58b0fc381fccd5118
Chris Guard <cguard90@gmail.com>
2016-02-15 22:24:42 -0500

    rails up the routes

--- blog2/config/routes.rb
+++ blog2/config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
 
   root 'welcome#index'
-  get 'links' => 'links#index'
   resources :entries,  only: [:show]
+  resources :links, only: [:index]
+  resources :projects, only: [:index]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
